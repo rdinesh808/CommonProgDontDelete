5c7fab6bb231f3961289306c874b6d12a6e66a3f
prabhaharan <prabhaharan.velu@gsihealth.com>
2019-12-13 17:04:42 +0530

    newstringprog

---
+++ src/main/java/com/logicalprog/ReverseStringUsingIndex.java
@@ -0,0 +1,15 @@
+package com.logicalprog;
+import java.util.*;
+public class ReverseStringUsingIndex {
+	public static void main(String args[]) {
+	      String str = "New England";
+	      StringBuffer sb= new StringBuffer(str);
+	      sb.reverse();
+	      for(int i=0 ; i<str.length(); i++){
+	      if(str.charAt(i) == (char)32){
+	         sb.insert(i, " ");
+	      }
+	   }
+	   System.out.println(sb);
+	}
+}
